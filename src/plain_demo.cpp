374f76e5d9e7663662677050e4030d49f5010376
Torbjörn Rathsman <milasudril@gmail.com>
2023-04-09 20:18:10 +0200

    Make it possible to load wavefront data

--- src/plain_demo.cpp
+++ src/plain_demo.cpp
@@ -5,17 +5,130 @@
 #include <array>
 #include <cassert>
 #include <algorithm>
+#include <optional>
 
 struct face_indirect
 {
   std::array<uint32_t, 3> vertices;
 };
 
+std::vector<Point3D> load_points(FILE* stream)
+{
+  std::vector<Point3D> ret;
+  enum class state{newline, skipline, vertex_begin, coords};
+  auto current_state = state::newline;
+
+  std::string buffer;
+  size_t fieldcount = 0;
+  Point3D point;
+
+  while(true)
+  {
+    auto const ch_in = getc(stream);
+    if(ch_in == EOF)
+    {
+      if(fieldcount != 0)
+      { ret.push_back(point); }
+
+      return ret;
+    }
+
+    switch(current_state)
+    {
+      case state::newline:
+        switch(ch_in)
+        {
+          case '#':
+            current_state = state::skipline;
+            break;
+          case 'v':
+            current_state = state::vertex_begin;
+            break;
+          case 'f':
+            current_state = state::skipline;
+            break;
+          case 'l':
+            current_state = state::skipline;
+            break;
+          default:
+            throw std::runtime_error{"Invalid line"};
+        }
+        break;
+      case state::skipline:
+        if(ch_in == '\n')
+        { current_state = state::newline; }
+        break;
+
+      case state::vertex_begin:
+        switch(ch_in)
+        {
+          case ' ':
+            current_state = state::coords;
+            break;
+          case 't':
+            current_state = state::skipline;
+            break;
+          case 'p':
+            current_state = state::skipline;
+            break;
+          case 'n':
+            current_state = state::skipline;
+            break;
+          default:
+            throw std::runtime_error{"Invalid line"};
+        }
+        break;
+
+      case state::coords:
+        switch(ch_in)
+        {
+          case ' ':
+            switch(fieldcount)
+            {
+              case 0:
+                point.x = std::stof(buffer);
+                break;
+              case 1:
+                point.y = std::stof(buffer);
+                break;
+              case 2:
+                point.z = std::stof(buffer);
+                break;
+            }
+            buffer.clear();
+            ++fieldcount;
+            break;
+
+          case '\n':
+            switch(fieldcount)
+            {
+              case 0:
+                point.x = std::stof(buffer);
+                break;
+              case 1:
+                point.y = std::stof(buffer);
+                break;
+              case 2:
+                point.z = std::stof(buffer);
+                break;
+            }
+            buffer.clear();
+            fieldcount = 0;
+            ret.push_back(point);
+            current_state = state::newline;
+            break;
+
+          default:
+            buffer += static_cast<char>(ch_in);
+        }
+        break;
+    }
+  };
+}
+
 int main()
 {
-  std::vector<Point3D> tests = \
-    {{3,0,0}, {0,3,0}, {0,0,3}, {3,3,3}};
-  ConvexHull C(tests);
+  ConvexHull C(load_points(stdin));
 
   std::unordered_map<Point3D, uint32_t, PointHash> vertex_index;
   std::vector<Point3D> sorted_verts;
